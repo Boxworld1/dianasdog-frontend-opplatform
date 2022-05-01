3fe0ad57b35acfab2b77b2150cef5ec7acbf1d20
马钿雨 <2020010881@secoder.net>
2022-05-02 02:50:31 +0800

    添加label的dialog #7

--- src/views/WScopy.vue
+++ src/views/WScopy.vue
@@ -7,25 +7,25 @@
         <el-table-column prop="es" label="是否插入es">
           <template slot-scope="scope">
             <el-radio-group v-model="scope.row.es">
-              <el-radio-button label="是"></el-radio-button>
-              <el-radio-button label="否"></el-radio-button>
+              <el-radio-button :label="true">是</el-radio-button>
+              <el-radio-button :label="false">否</el-radio-button>
             </el-radio-group>
           </template>
         </el-table-column>
         <el-table-column prop="redis" label="是否插入redis">
           <template slot-scope="scope">
-            <el-radio-group v-model="scope.row.es">
-              <el-radio-button label="是"></el-radio-button>
-              <el-radio-button label="否"></el-radio-button>
+            <el-radio-group v-model="scope.row.redis">
+              <el-radio-button :label="true">是</el-radio-button>
+              <el-radio-button :label="false">否</el-radio-button>
             </el-radio-group>
           </template>
         </el-table-column>
 
         <el-table-column prop="sql" label="是否插入sql">
           <template slot-scope="scope">
-            <el-radio-group v-model="scope.row.es">
-              <el-radio-button label="是"></el-radio-button>
-              <el-radio-button label="否"></el-radio-button>
+            <el-radio-group v-model="scope.row.sql">
+              <el-radio-button :label="true">是</el-radio-button>
+              <el-radio-button :label="false">否</el-radio-button>
             </el-radio-group>
           </template>
         </el-table-column>
@@ -45,6 +45,49 @@
         </el-table-column>
       </el-table>
     </div>
+    <div class="foot">
+      <template>
+        <el-button type="primary" @click="dialogLabelVisible = true">增加标签</el-button>
+        <el-button type="primary" @click="dialogResourceVisible = true"
+          >增加特性卡片</el-button
+        >
+      </template>
+    </div>
+    <el-dialog title="增加标签" :visible.sync="dialogLabelVisible">
+      <el-form :model="formLabel">
+        <el-form-item label="标签名字">
+          <el-input v-model="formLabel.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="是否插入es">
+          <el-radio-group v-model="formLabel.es">
+            <el-radio :label="true">是</el-radio>
+            <el-radio :label="false">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+                <el-form-item label="是否插入redis">
+          <el-radio-group v-model="formLabel.redis">
+            <el-radio :label="true">是</el-radio>
+            <el-radio :label="false">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="是否插入sql">
+          <el-radio-group v-model="formLabel.sql">
+            <el-radio :label="true">是</el-radio>
+            <el-radio :label="false">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogLabelVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitDialogLabel"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
+    <el-dialog title="增加特性卡片" :visible.sync="dialogResourceVisible">
+
+    </el-dialog>
   </div>
 </template>
 
@@ -60,7 +103,16 @@ export default {
     return {
       options: [],
       value_option: "",
-      labels: [{ name: "car", es: "是", redis: "是", sql: "否" }],
+      labels: [{ name: "car", es: true, redis: true, sql: false }],
+      dialogLabelVisible: false,
+      dialogResourceVisible: false,
+      //formLabel用于新增label
+      formLabel: {
+        name: "",
+        es: true,
+        redis: true,
+        sql: true,
+      },
     };
   },
   methods: {
@@ -70,6 +122,22 @@ export default {
     },
     handleDelete(index, row) {
       console.log(index, row);
+    },
+    resetFormLabel() {
+      this.formLabel={
+        name: "",
+        es: true,
+        redis: true,
+        sql: true,
+      }
+    },
+    submitDialogLabel() {
+      this.dialogLabelVisible = false;
+      this.labels.push(this.formLabel);
+      this.resetFormLabel();
+    },
+    submitDialogResource() {
+      this.dialogResourceVisible = true;
     },
   },
   watch: {
@@ -82,3 +150,9 @@ export default {
   },
 };
 </script>
+
+<style>
+.foot {
+  margin-top: 20px;
+}
+</style>
