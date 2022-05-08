d939e1ac9b055507399051b47db79f969e2f2d5d
容逸朗 <2020010869@secoder.net>
2022-05-08 18:02:34 +0800

    add unit test for .js #14

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,17 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
-
+import routes from "@/router/index.js";
+import store from "@/store/index.js";
+import admin from "@/store/modules/admin.js";
+import user from "@/store/modules/user.js";
 describe('App.vue', () => {
 
   it('renders props.msg when passed', () => {
     const wrapper = shallowMount(App, {
       global: {
         mocks: {
-          $store: {
-            state: {
-              user: "hi",
-            }
-          }
+          $store: store,
+          $router: routes,
+          $admin: admin,
+          $user: user,
         }
       }
     })
