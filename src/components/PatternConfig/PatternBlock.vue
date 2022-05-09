d4a1eb18049007a487e8a8c401c5c05503a8aae0
蒋政 <2020010880@secoder.net>
2022-05-09 12:25:36 +0800

    modify the patternblock #11

--- src/components/PatternConfig/PatternBlock.vue
+++ src/components/PatternConfig/PatternBlock.vue
@@ -1,11 +1,11 @@
 <template>
-  <div style="background: #b0c7e7; border-radius: 4px; margin: 10px">
+  <div style="background: #b0c7e7; border-radius: 4px; margin: 10px ">
     <span style="text-align: left"><i class="el-icon-s-promotion" />{{ rawpattern }}</span>
     <span>
       <!-- <el-button type="primary" icon="el-icon-edit" circle v-on:click="edit(rawpattern)"></el-button>
       <el-button type="danger" icon="el-icon-delete" circle v-on:click="deletepattern([rawpattern])"></el-button> -->
-      <el-button type="primary" v-on:click="edit(rawpattern)"><i class="el-icon-edit" />Edit</el-button>
-      <el-button type="danger" v-on:click="deletepattern([rawpattern])"><i class="el-icon-delete" />Delete</el-button>
+      <el-button style="float: right" type="danger" v-on:click="deletepattern([rawpattern])"><i class="el-icon-delete" />Delete</el-button>
+      <el-button style="float: right" type="primary" v-on:click="edit(rawpattern)"><i class="el-icon-edit" />Edit</el-button>
     </span>
   </div>
 </template>
